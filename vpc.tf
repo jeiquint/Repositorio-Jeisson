be58883582b0930369e21ab6674018795ff35842
jeiquint <78003061+jeiquint@users.noreply.github.com>
2024-08-29 18:26:14 -0500

    Update vpc.tf
    Se realiza la adición del código para la creación de los grupos de seguridad utilizados en el proyecto.

--- vpc.tf
+++ vpc.tf
@@ -47,3 +47,37 @@ resource "aws_subnet" "private3" {
     Name = "SNET_privado3"
   }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "allow_ssh"
+  vpc_id      = aws_vpc.vpc_proyecto.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Habilitar_SSH"
+  }
+}
